91fc8cf283ffa840adf1e99af90c44ab62d8a426
Eric Crescioni <ecam900@gmail.com>
2016-01-31 20:34:43 +0000

    Add maps API

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,14 @@
 class Place < ActiveRecord::Base
+	#Kamari Pagination-----
 	paginates_per 5
 	belongs_to :user
-	validates :name, presence: true
+	
+	#Geocoding-------------
+	geocoded_by :address
+	after_validation :geocode
+	
+	#Validation-------------
+	validates :name, :length => { minimum: 3 }, :presence => true
+	validates :address, :length => { minimum: 10}, :presence => true
+	validates :description, :length => { minimum: 15}, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130194217) do
+ActiveRecord::Schema.define(version: 20160131191701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20160130194217) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
